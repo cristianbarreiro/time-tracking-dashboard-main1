74ec4023d0b898b7ee40bbef47829076b1eb3fdd
Cristian <cb2onix@gmail.com>
2025-05-24 22:54:09 -0300

    feat: updates

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const times = document.querySelectorAll(".times p");
+const container = document.querySelector(".times");
+
+// Inicially mark as active the first element with the "active" class.
+let activeElement = document.querySelector(".times p.active") || times[0];
+activeElement.classList.add("active");
+
+times.forEach((p) => {
+  p.addEventListener("mouseenter", () => {
+    // Drop active
+    times.forEach((el) => el.classList.remove("active"));
+    // add actual active
+    p.classList.add("active");
+    // save like new active
+    activeElement = p;
+  });
+});
+
+container.addEventListener("mouseleave", () => {
+  // keep the last hovered element as active
+  times.forEach((el) => el.classList.remove("active"));
+  activeElement.classList.add("active");
+});
